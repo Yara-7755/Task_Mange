ac7aa23436eddeae1968f702b5be6970e3f16ce5
abusnainehtasneem <abusnainehtasneem@gmail.com>
2026-08-18 11:53:11 +0300

    fixed error in show my task

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\tasks\CreateTaskController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -11,6 +11,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/logout-all', [AuthController::class, 'logoutAll']);
-
+    Route::get('/tasks', [TaskController::class, 'index']);
     Route::apiResource('tasks', TaskController::class);
-});
+    Route::post('/tasks', [CreateTaskController::class, 'store']);});
